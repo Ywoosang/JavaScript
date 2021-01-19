be5ce3f86be3f43870c8bdb7c9af5bcc4f2f8065
Ywoosang <opellong13@gmail.com>
2021-01-19 10:59:21 +0900

    Add how to use file system in node

---
+++ node/process.js
@@ -0,0 +1,6 @@
+// process.exit(1) 서버에서 오류 있을 때 확인, 프로세스 종료하는 용도로 사용
+// process.exit(1) 오류 알려주고 종료 , process.exit(0) 오류 안알려주고 종료
+process.pwd() // 현재 경로
+process.uptime() // 프로세스 실행 후 얼마나 시간이 지났는지
+process.cpuUsage() // 프로세스 cpu 메모리 
+// process 부분 확인
