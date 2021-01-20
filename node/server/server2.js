a19042c62e488d082ed8a0328bd649589cd75055
Ywoosang <opellong13@gmail.com>
2021-01-20 12:01:37 +0900

    Add render html

---
+++ node/server/server2.js
@@ -0,0 +1,22 @@
+// html 렌더링 방법 
+const http = require('http');
+const fs = require('fs').promises; // async 로 하면 promises 꼭 붙여주기 
+
+const server = http.createServer(async (req,res) => { // async 함수로 바꿔줌 
+    try{
+        res.writeHead(200,{'Content-Type': 'text/html; charset=utf-8'}); // html 로 인식 가능하도록 text/hltm, utf-8 인코딩 한글 안깨지도록
+        const data = await fs.readFile('./server2.html'); // html 파일을 읽어서 전송  
+        console.log(data);
+        res.end(data); 
+    } catch(err){
+        console.error(err);
+        res.writeHead(200,{'Content-Type': 'text/plane; charset=utf-8'}); // 일반 문자열 text/plane
+        res.end(err.message); // 에러가 난 경우에 에러 메시지를 확인할 수 있도록 
+    }
+})
+    .listen(8080,()=> {
+    console.log('8080번 포트에서 서버 대기중')
+});   
+server.on('error',(error)=>{
+    console.error(error)
+})
