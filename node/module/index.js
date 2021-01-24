bda550cb8bdb09045dbbdf3794d1cd61706d92fc
Ywoosang <opellong13@gmail.com>
2021-01-25 00:23:05 +0900

    Refactor variables

--- node/module/index.js
+++
@@ -1,6 +0,0 @@
-// 가져오는 것이기 떄문에 기존 파일에 console.log() 가 있었다면 실행되어 값이 같이 찍힘. 
-
-const checkNumber = require('./func')['checkOddorEven']; 
-console.log(checkNumber)
-const {odd,even} = require('./var');
-console.log(odd,even);
